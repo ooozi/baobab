4a529598e1f21724b4066c5f2129da10e5434759
odabari <ryu008@nate.com>
2021-11-29 22:36:29 +0900

    test

---
+++ cicd/github-cicd-action/src/test/java/com/my/springboot/cicd/githubcicdaction/GithubCicdActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.springboot.cicd.githubcicdaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubCicdActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
